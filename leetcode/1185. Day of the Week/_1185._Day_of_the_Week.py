e9376d507075169773d39f019726782ce8b4ed5c
acromat1c <i-reseland@outlook.com>
2024-08-21 23:45:09 +0200

    Delete leetcode directory

--- leetcode/1185. Day of the Week/_1185._Day_of_the_Week.py
+++
@@ -1,8 +0,0 @@
-import datetime
-import calendar
-
-class Solution:
-    def dayOfTheWeek(self, day: int, month: int, year: int) -> str:
-        return ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"][datetime.datetime.strptime(str(day)+" "+str(month)+" "+str(year), '%d %m %Y').weekday()]
-    
-print(Solution().dayOfTheWeek(5,3,2011))
