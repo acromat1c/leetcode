4b1e810e8b98883f602242b7441badc7e1d00971
acromat1c <i-reseland@outlook.com>
2024-08-22 17:35:32 +0200

    Add files via upload

---
+++ _3._Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s)==0: return 0
+        a=0
+        u=[]
+        n=0
+        for l in s:
+            if l in u:
+                n=len(u)
+                if n>a:a=n
+                u=u[u.index(l)+1:]
+            u.append(l)
+        n=len(u)
+        if n>a:return n
+        return a
+print(Solution().lengthOfLongestSubstring("aukysdfgyawuendyuewfeyyfwl"))
